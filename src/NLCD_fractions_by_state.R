01557a46117ac77e72fc18bcedb975eb5fcf4951
kristian Hajny <kristian.hajny@stonybrook.edu>
2024-05-15 19:47:02 -0400

    Remove commented out code.

--- src/NLCD_fractions_by_state.R
+++ src/NLCD_fractions_by_state.R
@@ -3,58 +3,11 @@
 
 NLCD_open_and_low_int <- function(){
   
-  # ################################################################################
-  # #Manually defined variables
-  # 
-  # Input_directory <- "G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Raw data/"
-  # Output_directory <- "G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed/"
-  # 
-  # d03_bounding_box <- cbind(c(-76.65,-73.65),c(38.97,40.97))
-  # resolution <- 0.01
-  # #Will be used to generate a blank raster for the output to be built onto, WGS84
-  # #CRS.  Resolution in deg, bounding box in lat/long.
-  # 
-  # State_subset <- c("DE", "MD", "NJ", 'NY', "PA")
-  # #state acronyms for the domain of interest
-  # 
+  ################################################################################
+  #Manually defined variables
   nlcd_file <- file.path("G:/My Drive/Shepson Group Drive/General Inventories and Shapefiles/Shapefiles",
                          "nlcd_2019_land_cover_l48_20210604/nlcd_2019_land_cover_l48_20210604.img")
-  #national land cover data at 30 m (https://www.usgs.gov/centers/eros/science/national-land-cover-database)
-  # 
-  # state_shapefile <- "G:/My Drive/Shepson Group Drive/General Inventories and Shapefiles/Shapefiles/tl_2022_us_state/tl_2022_us_state.shp"
-  # #Census tigerline shapefiles for states
-  # 
-  # XESMF_check <- TRUE
-  # #use xesmf to reproject (TRUE), or projectraster (FALSE)
-  # ################################################################################
-  # #load packages
-  # i <- 1
-  # packagecheck <- c("raster","sf")
-  # while(i<=length(packagecheck)){
-  #   if(length(find.package(packagecheck[i],quiet = TRUE))<1){
-  #     install.packages(packagecheck[i],repos="https://repo.miserver.it.umich.edu/cran/")
-  #   }
-  #   i <- i+1
-  # }
-  # 
-  # invisible(suppressPackageStartupMessages(lapply(packagecheck, require, character.only=TRUE)))
-  # rm(packagecheck,i)
-  # 
-  # #raster = raster and data functionalities
-  # #sf = simple features, adds spatial functionalities
-  # ################################################################################
-  # #now quickly build the output raster matrix and load in the raster NLCD data
-  # 
-  # d03_rast <- raster(nrows=diff(range(d03_bounding_box[,2]))/resolution, 
-  #                    ncols=diff(range(d03_bounding_box[,1]))/resolution,
-  #                    xmn=min(d03_bounding_box[,1]), xmx=max(d03_bounding_box[,1]),
-  #                    ymn=min(d03_bounding_box[,2]), ymx=max(d03_bounding_box[,2]), 
-  #                    crs=4326)
-  # 
-  # rm(d03_bounding_box,resolution)
-  # 
-  # nlcd <- brick(nlcd)
-  # 
+  
   ################################################################################
   #subset NLCD to the appropriate states
   
@@ -124,7 +77,6 @@ NLCD_open_and_low_int <- function(){
   
   for(A in 1:length(state_name_list)){
     #mask to just the 1 state
-    
     subset_open <- mask(reprojected_open,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]])
     subset_low_int <- mask(reprojected_low_int,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]])
     #save
@@ -147,4 +99,7 @@ NLCD_open_and_low_int <- function(){
   #save state-level totals
   write.csv(area_df,file.path(output_directory,'nlcd_state_total_areas.csv'))
   
+  # plot(septic_emiss2,ylim=c(39.5,39.8),xlim=c(-75.85,-75.7))
+  # abline(v=-75.77);abline(v=-75.78);abline(v=-75.79)
+  ## DE and MD overlap here, so they can be compared more clearly.
 }
