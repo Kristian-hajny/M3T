e62a5af44bb35f7db34e98da3b87979ccaf2601c
kristian Hajny <kristian.hajny@stonybrook.edu>
2026-01-06 21:29:20 -0600

    switching to examplesif for CRAN, switching from methods::is to isa as isa is a base equivalent as used here

--- R/CH4_inventory_build.R
+++ R/CH4_inventory_build.R
@@ -54,7 +54,7 @@
 #'  updates as multiple other functions are run and output files created for the
 #'  various sectors.
 #'@export
-#'@examples
+#'@examplesIf interactive() && curl::has_internet()
 #' CH4_inventory_build(run_directory=tempdir(),
 #'                     inventory_year=2019,
 #'                     domain="RI",
@@ -111,13 +111,13 @@ CH4_inventory_build <- function(run_directory,
     #only include row names if they exist, not just row numbers (confusing to
     #read).  Use print for tables/lists (outputs ~formatted) and cat for
     #number/text.
-    if(methods::is(temp_data,"data.frame")){
+    if(isa(temp_data,"data.frame")){
       if(rownames(temp_data)[1]=="1"){
         print(temp_data,quote = FALSE,row.names=F,width=300)
       }else{
         print(temp_data,quote = FALSE,width=300)
       }
-    }else if(methods::is(temp_data,"list")){
+    }else if(isa(temp_data,"list")){
       print(temp_data,quote = FALSE,width=300)
     }else{
       cat(temp_data)
@@ -135,13 +135,13 @@ CH4_inventory_build <- function(run_directory,
     #only include row names if they exist, not just row numbers (confusing to
     #read).  Use print for tables/lists (outputs ~formatted) and cat for
     #number/text.
-    if(methods::is(temp_data,"data.frame")){
+    if(isa(temp_data,"data.frame")){
       if(rownames(temp_data)[1]=="1"){
         print(temp_data,quote = FALSE,row.names=F,width=300)
       }else{
         print(temp_data,quote = FALSE,width=300)
       }
-    }else if(methods::is(temp_data,"list")){
+    }else if(isa(temp_data,"list")){
       print(temp_data,quote = FALSE,width=300)
     }else{
       cat(temp_data)
@@ -248,14 +248,14 @@ CH4_inventory_build <- function(run_directory,
   #ID any source options that are 0 characters or not text
   problem_source_entries <- names(M3T_config)[grepl(names(M3T_config),pattern="Source_.*")]
   problem_source_entries <- problem_source_entries[sapply(problem_source_entries,M3T_get_config)==0 | 
-                                                     sapply(problem_source_entries,function(x){!methods::is(M3T_get_config(x),"character")})]
+                                                     sapply(problem_source_entries,function(x){!isa(M3T_get_config(x),"character")})]
   if(length(problem_source_entries)>0){
     error_found <- TRUE
     problem_source_entries <- paste(problem_source_entries,collapse=", ")
     error_text <- paste0(error_text,"\n\nMust set all data sources to \"default\", \"download\", or a file path. ",problem_source_entries," are set incorrectly (either not text in \"\" or empty).")
   }
   
-  if(M3T_config$Process_landfills & !(methods::is(M3T_config$GHGI_landfill_total,"numeric") | M3T_config$GHGI_landfill_total=="GHGI")){
+  if(M3T_config$Process_landfills & !(isa(M3T_config$GHGI_landfill_total,"numeric") | M3T_config$GHGI_landfill_total=="GHGI")){
     error_found <- TRUE
     error_text <- paste0(error_text,"\n\nMust set M3T_config$Process_landfills to FALSE or set M3T_config$GHGI_landfill_total in config to a number or \"GHGI\"")
   }
@@ -454,14 +454,14 @@ CH4_inventory_build <- function(run_directory,
     domain_res <- rep(domain_res,2)
   }
   
-  if(methods::is(domain,"data.frame")){
+  if(isa(domain,"data.frame")){
     domain <- terra::rast(nrows=diff(range(domain[,2]))/domain_res[2],
                           ncols=diff(range(domain[,1]))/domain_res[1],
                           xmin=min(domain[,1]), xmax=max(domain[,1]),
                           ymin=min(domain[,2]), ymax=max(domain[,2]),
                           vals=1)
     domain <- terra::as.polygons(terra::ext(domain),crs=domain_crs)
-  }else if(methods::is(domain,"character")){
+  }else if(isa(domain,"character")){
     #if multiple entries, focus on just 1 for simpler comparisons (must all be
     #the same type anyway)
     test_domain <- domain[1]
